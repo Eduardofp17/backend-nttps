85c614e236043a02d69912c51704e85df3d27711
Eduardofp <edardofernandespinheiro@gmail.com>
2023-04-22 15:32:01 -0300

    (fix): fixing bug on build function

--- dist/src/routes/email.js
+++
@@ -1,7 +0,0 @@
-"use strict";"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var email_1 = require("../controllers/email");
-var router = new express_1.Router();
-router.post('/', email_1.default.create);
-exports.default = router;
--- dist/src/routes/tokenRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var token_1 = require("../controllers/token");
-var router = new express_1.Router();
-router.post("/", token_1.default.create);
-exports.default = router;
