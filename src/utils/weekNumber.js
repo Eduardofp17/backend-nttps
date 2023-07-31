1737d279ab72bad0b6467942d6ec9b075ee962a5
Eduardofp <edardofernandespinheiro@gmail.com>
2023-07-30 22:37:47 -0300

    [BUG] fixing bug that alg returns in wrong week

--- src/utils/weekNumber.js
+++ src/utils/weekNumber.js
@@ -1,33 +1,21 @@
 class Semanas {
-  static getWeekNumOfMonthOfDate(d) {
+  static getWeekNumOfDate(d) {
     const onejan = new Date(d.getFullYear(), 0, 1);
-    const year = d.getFullYear();
-    const week = Math.ceil(
-      (((d.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7,
-    );
-    if (week === 22) {
-      return week;
-    }
-    return week + 1;
+    const daysInYear = Math.floor((d - onejan) / (24 * 60 * 60 * 1000));
+    const weekNum = Math.ceil((daysInYear + onejan.getDay() + 1) / 7);
+    return weekNum;
+  }
+
+  static isOdd(number) {
+    return number % 2 !== 0;
   }
 
   static pegarDataAtual() {
-    const today = new Date();
-    const yy = today.getFullYear();
-    const mm = today.getMonth();
-    const dd = today.getDate();
-    this.weekNumOfDate = this.getWeekNumOfMonthOfDate(new Date(yy, mm, dd));
-    this.semana = this.verificaSeaSemanaEimpar(this.weekNumOfDate);
-    return this.semana;
-  }
-
-  static verificaSeaSemanaEimpar(data) {
-    if (data % 2 === 0) {
-      return 0;
-    } if (data % 2 === 1) {
-      return 1;
-    }
-    return new Error('[ERROR]');
+    const today = new Date('2023-08-28');
+    const weekNumOfDate = this.getWeekNumOfDate(today);
+    const semana = this.isOdd(weekNumOfDate) ? 1 : 0;
+    return semana;
   }
 }
+
 module.exports = Semanas;
